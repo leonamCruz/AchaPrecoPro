5c49eba5c1cfd52a0ceeb9281d8b9db665fb1c34
leonam <leonam447@gmail.com>
2024-05-30 22:43:54 -0300

    Entidade Criada

--- src/main/java/tech/leonam/achaprecopro/model/ProdutoEntity.java
+++ src/main/java/tech/leonam/achaprecopro/model/ProdutoEntity.java
@@ -1,4 +1,25 @@
 package tech.leonam.achaprecopro.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
 public class ProdutoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nomeDoProduto;
+    private String descricaoDoProduto;
+    private String codBarras;
+    private String price;
+    private String localizacaoDaImagem;
+    private LocalDateTime ultimaAlteracao;
+
 }
