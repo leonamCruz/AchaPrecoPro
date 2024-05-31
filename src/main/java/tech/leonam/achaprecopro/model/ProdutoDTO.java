199698899a6479e8e7ac5bb3b6d5876352382911
leonam <leonam447@gmail.com>
2024-05-30 23:08:15 -0300

    DTO ok

--- src/main/java/tech/leonam/achaprecopro/model/ProdutoDTO.java
+++ src/main/java/tech/leonam/achaprecopro/model/ProdutoDTO.java
@@ -1,4 +1,22 @@
 package tech.leonam.achaprecopro.model;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class ProdutoDTO {
+    @NotBlank
+    private String nomeDoProduto;
+    @NotBlank
+    private String descricaoDoProduto;
+    @NotBlank
+    private String codBarras;
+    @NotBlank
+    private String price;
+    @NotBlank
+    private String localizacaoDaImagem;
+    @NotBlank
+    private LocalDateTime ultimaAlteracao;
 }
